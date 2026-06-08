55673600bd7ea2ad406e1cc00cb7f3981256a293
Gary Pinsky <kd8tbs@gmail.com>
2026-06-08 16:56:28 -0600

    Add standalone overrides file

---
+++ overrides.js
@@ -0,0 +1,22 @@
+/****************************************************************************
+ * MY OVERRIDES
+ ****************************************************************************/
+user_pref("permissions.default.geo", 0);
+user_pref("permissions.default.desktop-notification", 0);
+
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", true);
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+
+user_pref("browser.newtabpage.activity-stream.showWeather", true);
+user_pref("browser.newtabpage.activity-stream.system.showWeather", true);
+
+user_pref("browser.urlbar.formatting.enabled", true);
+
+user_pref("browser.ml.enable", true);
+user_pref("browser.ml.chat.enabled", true);
+user_pref("browser.ml.chat.menu", true);
+user_pref("browser.tabs.groups.smart.enabled", true);
+user_pref("browser.ml.linkPreview.enabled", true);
